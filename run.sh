96edfb7f8124705b9f46934da441139823a507df
minhajkhan75 <minhajuddinkhan75@gmail.com>
2017-10-14 12:42:53 +0500

    bash

--- run.sh
+++ run.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 OS=`uname`;
-echo $OS
-
-
-if [ "$1" == "-s" ]; then
-   cd $HOME 
-   git clone https://github.com/minhajuddinkhan/twenty20rule
-   cd twenty20rule 
-   chmod +x twenty20rule
-   
-   if [ $OS == "Linux" ]; then 
-        echo "INSIDo!"
+chmod +x twenty20rule   
+if [ $OS == "Linux" ]; then 
         sudo cp twenty20rule /usr/bin   
         sudo echo "/usr/bin/twenty20rule &" >> /etc/rc.local  
         twenty20rule &
-   fi     
-else
-   go get github.com/minhajuddinkhan/twenty20rule 
-   if [ $OS == "Linux" ]; then 
-      twenty20rule &
-      sudo echo "$GOPATH/bin/twenty20rule" >> /etc/rc.local    
-   fi         
-fi
+fi     
 
+if [$OS == "Darwin"]; then
+
+    echo "do something for osx"
+
+fi
